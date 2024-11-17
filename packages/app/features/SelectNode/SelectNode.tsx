cc2664e236627f07c0431ddc01c34a928cd3368a
Christopher DeVito <christopher.devito@protonmail.com>
2024-11-17 14:08:37 -0500

    Minium page to find error on vercel

--- packages/app/features/SelectNode/SelectNode.tsx
+++ packages/app/features/SelectNode/SelectNode.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { FlatList, Pressable, View } from 'dripsy';
-import { Node } from 'app/features/SelectNode/selectNodeRowTypes';
-import { useRouter } from 'solito/router';
+// import { Node } from 'app/features/SelectNode/selectNodeRowTypes';
+// import { useRouter } from 'solito/router';
 import Header from 'app/features/components/Header/Header';
-import SegmentedControl from 'app/features/components/SegmentedControl/SegmentedControl';
-import SearchBar from 'app/features/components/SearchBar/SearchBar';
-import { Cog6Tooth } from '@nandorojo/heroicons/24/solid';
-import SelectNodeRow from 'app/features/SelectNode/SelectNodeRow';
-import { secondsUntil } from 'app/features/SharedHooks/useTime';
-import BotanyNodes from 'app/features/Data/botany.json';
-import MiningNodes from 'app/features/Data/mining.json';
-import FishingNodes from 'app/features/Data/fishing.json';
+// import SegmentedControl from 'app/features/components/SegmentedControl/SegmentedControl';
+// import SearchBar from 'app/features/components/SearchBar/SearchBar';
+// import { Cog6Tooth } from '@nandorojo/heroicons/24/solid';
+// import SelectNodeRow from 'app/features/SelectNode/SelectNodeRow';
+// import { secondsUntil } from 'app/features/SharedHooks/useTime';
+// import BotanyNodes from 'app/features/Data/botany.json';
+// import MiningNodes from 'app/features/Data/mining.json';
+// import FishingNodes from 'app/features/Data/fishing.json';
 
 interface SelectNodesProps {
   profession: 'botany' | 'mining' | 'fishing';
 }
 
-interface Settings {
-  searchText: string;
-  expacIndex: number;
-  sortIndex: number;
-  currentNodes: Node[];
-}
+// interface Settings {
+//   searchText: string;
+//   expacIndex: number;
+//   sortIndex: number;
+//   currentNodes: Node[];
+// }
 
 // const loadNodeList = async (profession: string) => {
 //   let nodeList: Node[];
@@ -46,23 +46,23 @@ interface Settings {
 
 const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
   // const [nodeList, setNodeList] = useState<Node[]>([]);
-  const nodeList =
-    profession === 'botany'
-      ? BotanyNodes
-      : profession === 'mining'
-      ? MiningNodes
-      : profession === 'fishing'
-      ? FishingNodes
-      : [];
-  const [settings, setSettings] = useState<Settings>({
-    searchText: '',
-    expacIndex: 0,
-    sortIndex: 0,
-    currentNodes: nodeList,
-  });
-  const { push } = useRouter();
-  const maxTimeUntil = 24 * (2 * 60 + 55);
-  const thresholdTIme = 21 * (2 * 60 + 55);
+  // const nodeList =
+  //   profession === 'botany'
+  //     ? BotanyNodes
+  //     : profession === 'mining'
+  //     ? MiningNodes
+  //     : profession === 'fishing'
+  //     ? FishingNodes
+  //     : [];
+  // const [settings, setSettings] = useState<Settings>({
+  //   searchText: '',
+  //   expacIndex: 0,
+  //   sortIndex: 0,
+  //   currentNodes: nodeList,
+  // });
+  // const { push } = useRouter();
+  // const maxTimeUntil = 24 * (2 * 60 + 55);
+  // const thresholdTIme = 21 * (2 * 60 + 55);
 
   // useEffect(() => {
   //   const fetchNodeList = async () => {
@@ -77,153 +77,153 @@ const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
   //   fetchNodeList();
   // }, [profession]);
 
-  const handleSelectExpac = (expac: number, nodes: Node[]): Node[] => {
-    return expac === 0
-      ? nodes.filter((node: Node) => true)
-      : nodes.filter((node: Node) => node.expac === expac - 1);
-  };
+  // const handleSelectExpac = (expac: number, nodes: Node[]): Node[] => {
+  //   return expac === 0
+  //     ? nodes.filter((node: Node) => true)
+  //     : nodes.filter((node: Node) => node.expac === expac - 1);
+  // };
 
-  const handleSearchText = (text: string, nodes: Node[]): Node[] => {
-    if (!!text) {
-      var searchResults = nodes.filter((node: Node) => {
-        return node.name.toLowerCase().includes(text.toLowerCase());
-      });
-      return searchResults;
-    } else {
-      return nodes;
-    }
-  };
+  // const handleSearchText = (text: string, nodes: Node[]): Node[] => {
+  //   if (!!text) {
+  //     var searchResults = nodes.filter((node: Node) => {
+  //       return node.name.toLowerCase().includes(text.toLowerCase());
+  //     });
+  //     return searchResults;
+  //   } else {
+  //     return nodes;
+  //   }
+  // };
 
-  const sortByName = (a: Node, b: Node): number => {
-    const nameComparison = a.name.localeCompare(b.name);
-    return nameComparison;
-  };
+  // const sortByName = (a: Node, b: Node): number => {
+  //   const nameComparison = a.name.localeCompare(b.name);
+  //   return nameComparison;
+  // };
 
-  const sortBytime = (a: Node, b: Node): number => {
-    const aTime = a.time;
-    const bTime = b.time;
+  // const sortBytime = (a: Node, b: Node): number => {
+  //   const aTime = a.time;
+  //   const bTime = b.time;
 
-    if (aTime === undefined && bTime === undefined) return 0;
-    if (!!aTime && bTime === undefined) return -1;
-    if (aTime === undefined && !!bTime) return 1;
+  //   if (aTime === undefined && bTime === undefined) return 0;
+  //   if (!!aTime && bTime === undefined) return -1;
+  //   if (aTime === undefined && !!bTime) return 1;
 
-    if (aTime === bTime) {
-      return 0;
-    }
-    const timeUntilA = secondsUntil(aTime!);
-    const timeUntilB = secondsUntil(bTime!);
-    const isAAvailable =
-      timeUntilA <= maxTimeUntil && timeUntilA >= thresholdTIme;
-    const isBAvailable =
-      timeUntilB <= maxTimeUntil && timeUntilB >= thresholdTIme;
+  //   if (aTime === bTime) {
+  //     return 0;
+  //   }
+  //   const timeUntilA = secondsUntil(aTime!);
+  //   const timeUntilB = secondsUntil(bTime!);
+  //   const isAAvailable =
+  //     timeUntilA <= maxTimeUntil && timeUntilA >= thresholdTIme;
+  //   const isBAvailable =
+  //     timeUntilB <= maxTimeUntil && timeUntilB >= thresholdTIme;
 
-    // Check if only one or the other is currently available
-    if (isAAvailable && !isBAvailable) return -1;
-    if (!isAAvailable && isBAvailable) return 1;
+  //   // Check if only one or the other is currently available
+  //   if (isAAvailable && !isBAvailable) return -1;
+  //   if (!isAAvailable && isBAvailable) return 1;
 
-    return timeUntilA - timeUntilB;
-  };
+  //   return timeUntilA - timeUntilB;
+  // };
 
-  const sortByLocation = (a: Node, b: Node): number => {
-    return a.location.localeCompare(b.location);
-  };
+  // const sortByLocation = (a: Node, b: Node): number => {
+  //   return a.location.localeCompare(b.location);
+  // };
 
-  const handleSelectSort = (sort: number, nodes: Node[]): Node[] => {
-    var newNodes = nodes;
-    console.log('starting to sort: ', nodes.length);
-    switch (sort) {
-      case 0:
-        newNodes = newNodes.sort((a: Node, b: Node) => {
-          // Sort by name first and return if names don't match
-          const nameComparison = sortByName(a, b);
-          if (nameComparison !== 0) return nameComparison;
+  // const handleSelectSort = (sort: number, nodes: Node[]): Node[] => {
+  //   var newNodes = nodes;
+  //   console.log('starting to sort: ', nodes.length);
+  //   switch (sort) {
+  //     case 0:
+  //       newNodes = newNodes.sort((a: Node, b: Node) => {
+  //         // Sort by name first and return if names don't match
+  //         const nameComparison = sortByName(a, b);
+  //         if (nameComparison !== 0) return nameComparison;
 
-          // Sort by times if names are the same
-          // Sort items with time first and items without times after
-          const timeComparison = sortBytime(a, b);
-          if (timeComparison !== 0) return timeComparison;
+  //         // Sort by times if names are the same
+  //         // Sort items with time first and items without times after
+  //         const timeComparison = sortBytime(a, b);
+  //         if (timeComparison !== 0) return timeComparison;
 
-          // If neither has a time or times are the same, sort by location
-          return sortByLocation(a, b);
-        });
-        return newNodes;
-      case 1:
-        newNodes = newNodes.sort((a: Node, b: Node) => {
-          // Sort by Time first
-          const timeComparison = sortBytime(a, b);
-          if (timeComparison !== 0) return timeComparison;
+  //         // If neither has a time or times are the same, sort by location
+  //         return sortByLocation(a, b);
+  //       });
+  //       return newNodes;
+  //     case 1:
+  //       newNodes = newNodes.sort((a: Node, b: Node) => {
+  //         // Sort by Time first
+  //         const timeComparison = sortBytime(a, b);
+  //         if (timeComparison !== 0) return timeComparison;
 
-          // Sort by Name second
-          const nameComparison = sortByName(a, b);
-          if (nameComparison !== 0) return nameComparison;
+  //         // Sort by Name second
+  //         const nameComparison = sortByName(a, b);
+  //         if (nameComparison !== 0) return nameComparison;
 
-          // Sort by location last
-          return sortByLocation(a, b);
-        });
+  //         // Sort by location last
+  //         return sortByLocation(a, b);
+  //       });
 
-        return newNodes;
-      case 2:
-        newNodes = newNodes.sort((a: Node, b: Node) => {
-          // Sort by location first
-          const locationComparison = sortByLocation(a, b);
-          // return locationComparison;
-          if (locationComparison !== 0) return locationComparison;
+  //       return newNodes;
+  //     case 2:
+  //       newNodes = newNodes.sort((a: Node, b: Node) => {
+  //         // Sort by location first
+  //         const locationComparison = sortByLocation(a, b);
+  //         // return locationComparison;
+  //         if (locationComparison !== 0) return locationComparison;
 
-          // Sort by Name second
-          const nameComparison = sortByName(a, b);
-          // return nameComparison;
-          if (nameComparison !== 0) return nameComparison;
+  //         // Sort by Name second
+  //         const nameComparison = sortByName(a, b);
+  //         // return nameComparison;
+  //         if (nameComparison !== 0) return nameComparison;
 
-          // Sort by time last
-          return sortBytime(a, b);
-        });
-        return newNodes;
-      default:
-        return newNodes;
-    }
-  };
+  //         // Sort by time last
+  //         return sortBytime(a, b);
+  //       });
+  //       return newNodes;
+  //     default:
+  //       return newNodes;
+  //   }
+  // };
 
-  const handleSortAndSearch = (updates: Partial<Settings>) => {
-    const newSettings = {
-      ...settings,
-      ...updates,
-    };
-    const { expacIndex, sortIndex, searchText } = newSettings;
-    const nodes = nodeList;
+  // const handleSortAndSearch = (updates: Partial<Settings>) => {
+  //   const newSettings = {
+  //     ...settings,
+  //     ...updates,
+  //   };
+  //   const { expacIndex, sortIndex, searchText } = newSettings;
+  //   const nodes = nodeList;
 
-    // filter by expac
-    const expacNodes = handleSelectExpac(expacIndex, nodes);
+  //   // filter by expac
+  //   const expacNodes = handleSelectExpac(expacIndex, nodes);
 
-    // filter by search Text
-    const searchNodes = handleSearchText(searchText, expacNodes);
+  //   // filter by search Text
+  //   const searchNodes = handleSearchText(searchText, expacNodes);
 
-    // sort based on sort index
-    const sortedNodes = handleSelectSort(sortIndex, searchNodes);
+  //   // sort based on sort index
+  //   const sortedNodes = handleSelectSort(sortIndex, searchNodes);
 
-    setSettings({
-      expacIndex: expacIndex,
-      sortIndex: sortIndex,
-      searchText: searchText,
-      currentNodes: sortedNodes,
-    });
-  };
+  //   setSettings({
+  //     expacIndex: expacIndex,
+  //     sortIndex: sortIndex,
+  //     searchText: searchText,
+  //     currentNodes: sortedNodes,
+  //   });
+  // };
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout | undefined;
+  // useEffect(() => {
+  //   let timer: NodeJS.Timeout | undefined;
 
-    if (settings.sortIndex === 1) {
-      timer = setInterval(() => {
-        handleSortAndSearch({});
-      }, 5000);
-    }
+  //   if (settings.sortIndex === 1) {
+  //     timer = setInterval(() => {
+  //       handleSortAndSearch({});
+  //     }, 5000);
+  //   }
 
-    return () => {
-      if (timer) {
-        clearInterval(timer);
-      }
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [settings.sortIndex, settings.expacIndex, settings.searchText]);
+  //   return () => {
+  //     if (timer) {
+  //       clearInterval(timer);
+  //     }
+  //   };
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [settings.sortIndex, settings.expacIndex, settings.searchText]);
 
   return (
     <View
@@ -237,7 +237,7 @@ const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
       <Header />
 
       {/* Expansion Selection */}
-      <View sx={{ marginTop: 16, paddingX: 16 }}>
+      {/* <View sx={{ marginTop: 16, paddingX: 16 }}>
         <SegmentedControl
           title={'Expac:'}
           index={settings.expacIndex}
@@ -246,10 +246,10 @@ const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
           }
           values={['All', 'ARR', 'HW', 'SB', 'ShB', 'EW', 'DT']}
         />
-      </View>
+      </View> */}
 
       {/* Sort and filter Selection */}
-      <View
+      {/* <View
         sx={{
           flexDirection: 'row',
           width: '100%',
@@ -272,9 +272,9 @@ const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
         <Pressable onPress={() => {}}>
           <Cog6Tooth color="#666" />
         </Pressable>
-      </View>
+      </View> */}
 
-      <View
+      {/* <View
         sx={{
           height: 1,
           width: '100%',
@@ -292,17 +292,17 @@ const SelectNode: React.FC<SelectNodesProps> = ({ profession }) => {
           backgroundColor: '$lightGray',
           marginY: 6,
         }}
-      />
+      /> */}
 
       {/* Nodes List */}
-      <FlatList
+      {/* <FlatList
         data={settings.currentNodes}
         renderItem={({ item }) => <SelectNodeRow node={item as Node} />}
         keyExtractor={(item: unknown, index: number) => {
           const node = item as Node;
           return `${node.name}-${index}`;
         }}
-      />
+      /> */}
     </View>
   );
 };
